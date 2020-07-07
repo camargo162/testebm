a1d9f3a703f2fed71f5d343610d5316ceb35ef56
camargo162 <67983922+camargo162@users.noreply.github.com>
2020-07-07 20:03:52 -0300

    Add files via upload

---
+++ Cadastro1.aspx
@@ -0,0 +1,63 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cadastro1.aspx.cs" Inherits="Cadastro.Cadastro1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Cadastro Pessoa</title>
+    <style> 
+
+        .Container{
+            width: 450px;
+            margin: 0 auto;
+        }
+        .Container h1{
+            text-align: center;
+
+        }
+
+        .Campo{
+            padding: 10px;
+        }
+        .Campo input[type="text"]{
+            float: right;
+            width: 350px;
+        }
+        .Campo input[type="submit"]{
+            float: right;
+            width: 420px;
+            margin: 5px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="Container">
+            <h1>Cadastro de Pessoas</h1>
+            <div class="Campo">
+                <asp:label runat="server" AssociatedControlID="txtNome">Nome: </asp:label>
+                <asp:TextBox runat="server" ID="txtNome"></asp:TextBox>
+            </div>
+            <div class="Campo">
+                 <asp:label runat="server" AssociatedControlID="txtCPF">CPF: </asp:label>
+                <asp:TextBox runat="server" ID="txtCPF"></asp:TextBox>
+            </div>
+            <div class="Campo">
+                <asp:label runat="server" AssociatedControlID="txtIdade">Idade: </asp:label>
+                <asp:TextBox runat="server" ID="txtIdade"></asp:TextBox>
+            </div>
+            <div class="Campo">
+               <asp:Button runat="server" Text="Cadastrar" />
+                </div>
+                <div class="Campo">
+                
+                <asp:Button runat="server" Text="Lista" />
+        </div>
+ </div>
+  
+    </form>
+</body>
+</html>
+
+
---
+++ Cadastro1.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Cadastro
+{
+
+
+    public partial class Cadastro1
+    {
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle txtNome.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
+
+        /// <summary>
+        /// Controle txtCPF.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCPF;
+
+        /// <summary>
+        /// Controle txtIdade.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtIdade;
+    }
+}
